ec5c487aadab9d18475785473aca9f63e98312df
Tatyana <lioness1741@mail.ru>
2023-06-22 15:27:55 +0600

    dist exist

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _basic = _interopRequireDefault(require("./basic"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+// TODO: write your code here
+
+console.log('worked');
+console.log((0, _basic["default"])([1, 2]));
